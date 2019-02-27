dcdae72b5639dd132859e2f901ebeb6630330281
Kim-Ward <43408745+Kim-Ward@users.noreply.github.com>
2019-02-27 16:18:30 +0000

    Update 4_all_together.R

--- 4_all_together.R
+++ 4_all_together.R
@@ -1,39 +1,37 @@
 #' Brings previous work together.
 #' There are no intentional errors in this script!
+#' First of all, swap computers with a friend.
+#' Have you swapped? Good. Try not to break your friend's computer.
 
 source('2_vignere_style.R')
 source('3_one_time_pads.R')
 
 random_timestamp <- as.numeric(Sys.time())
-
 my_one_time_pad <- generate_one_time_pad(random_timestamp)
-
-save_one_time_pad(paste(random_timestamp, ".txt", sep=""))
+otp_name = paste(random_timestamp, ".txt", sep="")
+save_one_time_pad(my_one_time_pad, otp_name)
 
 #' Run the code above to get yourself a unique one-time pad.
-#' Send your one-time pad to a friend and receive theirs - 
-#' in practice, these were delivered by hand,
-#' under top secret conditions, well in advance of message sending.
-
-friends_one_time_pad <- load_one_time_pad('some_numbers.txt')
-
-#' Change the following string:
+#' Then run the code below, editing to add your own secret message.
 
 my_message <- "mysecretmessage"
-my_ciphertext <- crypto(my_message, my_one_time_pad)
-print(my_ciphertext)
+ciphertext <- crypto(my_message, my_one_time_pad)
+print(ciphertext)
+my_message <- ""
+my_one_time_pad <- ""
 
-#' Send the ciphertext to your friend, and have them send you theirs.
+#' Now swap back to your own computer.
+#' Have you swapped? Good. Welcome back!
 
-friends_ciphertext <- "whatevertheysentme"
-friends_message <- crypto(friends_ciphertext, friends_one_time_pad)
+friends_one_time_pad <- load_one_time_pad(otp_name)
+friends_message <- crypto(ciphertext, friends_one_time_pad)
 
 #' Can you decipher your friend's message? Does it all work?
 #' In practice, encrypted messages were sent using morse code over insecure radio waves.
 
 my_one_time_pad <- tear_off_page(my_one_time_pad)
+save_one_time_pad(my_one_time_pad, otp_name)
 
-#' You can tear off a page and send another message, up to 100 in total per pad.
-
+#' You can tear off a page and send another message if you want, up to 10 in total per pad.
 
 #' Finally: Commit your changes back to GitHub!
